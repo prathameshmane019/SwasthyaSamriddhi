cc1ba679121647bbafd5b20dfe3f74d9c5e923fb
prathameshmane019 <maneprathamesh019@gmail.com>
2024-03-31 00:23:26 +0530

    doctor pages

--- app/(main)/doctor/layout.js
+++ app/(main)/doctor/layout.js
@@ -3,7 +3,7 @@ import Sidebar from "@/app/components/doctorSidebar";
 export default function DoctorLayout({ children }) {
     return (    
         <div className="flex "><Sidebar/>
-        <div className="w-full h-full">
+        <div className="w-full h-full m-3">
             {children}
         </div>
        </div>
