7c0c176c426abfa63ec0621f82df83cd8cdb7ff7
12DeshmukhAish <113546938+12DeshmukhAish@users.noreply.github.com>
2024-03-30 22:04:24 +0530

    finddoctor added

---
+++ app/api/finddoctor/route.js
@@ -0,0 +1,20 @@
+import { connectMongoDB } from "@/app/libs/connectDb";
+import { NextResponse } from "next/server";
+import Doctor from "@/app/models/doctor";
+export async function POST(req) {
+    
+    try {
+        console.log("Trying")
+        connectMongoDB();
+        const id = await req.json();
+        const doctor = await Doctor.findOne({ _id: id });
+       
+        console.log(doctor);
+
+        return NextResponse.json(doctor);
+        
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({error});
+    }
+}
