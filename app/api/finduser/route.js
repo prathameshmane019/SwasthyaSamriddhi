b9fcac71563df93ace045d7f182d7a8ef9926822
prathameshmane019 <maneprathamesh019@gmail.com>
2024-03-30 23:24:58 +0530

    doctor pages

--- app/api/finduser/route.js
+++ app/api/finduser/route.js
@@ -2,16 +2,12 @@ import { connectMongoDB } from "@/app/libs/connectDb";
 import { NextResponse } from "next/server";
 import User from "@/app/models/user";
 export async function POST(req) {
-    console.log("Trying")
-   
     try {
         connectMongoDB();
         const id = await req.json();
         const user = await User.findOne({ _id: id });
-        await newUser.save()
-        console.log("user registered");
-    return NextResponse.json({message:"User registered"});
-        
+        console.log(user);
+    return NextResponse.json(user);
     } catch (error) {
         console.log(error);
         return NextResponse.json(error);
