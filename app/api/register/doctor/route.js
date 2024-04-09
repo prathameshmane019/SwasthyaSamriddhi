b69e463b9df19b0069ebbdb2e06cd037e89a637b
prathameshmane019 <maneprathamesh019@gmail.com>
2024-04-09 13:20:32 +0530

    alert messages added

--- app/api/register/doctor/route.js
+++ app/api/register/doctor/route.js
@@ -3,29 +3,8 @@ import { NextResponse } from "next/server";
 import Doctor from "@/app/models/doctor";
 import { sendRegistrationDoctorEmail } from "../../nodemailer/route";
 
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      await connectMongoDB();
-      const doctors = await Doctor.find();
-
-      if (doctors.length > 0) {
-        const doctorList = doctors.map((doctor) => ({
-          id: doctor._id,
-          name: `${doctor.fullname.firstName} ${doctor.fullname.middleName ? doctor.fullname.middleName + ' ' : ''}${doctor.fullname.surName}`,
-          email: doctor.email,
-          mobile: doctor.mobile,
-          specialization: doctor.specialization,
-        }));
-        return res.status(200).json(doctorList);
-      } else {
-        return res.status(404).json({ message: "Doctor(s) not found" });
-      }
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: "Internal server error" });
-    }
-  } else if (req.method === 'POST') {
+export async function POST(req) {
+    console.log("Trying")
     try {
       await connectMongoDB();
       const doctorData = await req.json();
@@ -37,12 +16,9 @@ export default async function handler(req, res) {
       await sendRegistrationDoctorEmail({ id:newDoctor._id,email: doctorData.email, name: name});
 
       console.log("Doctor registered successfully ", newDoctor);
-      return NextResponse.json(res, { message: "Doctor registered", doctor: newDoctor });
+      return NextResponse.json( { message: "Doctor registered", doctor: newDoctor });
     } catch (error) {
       console.error(error);
       return NextResponse.json(res, { error });
     }
-  } else {
-    return res.status(405).end(); 
-  }
 }
