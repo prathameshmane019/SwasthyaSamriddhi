df9eac22ace594406ed152bcba950af30a012eb3
prathameshmane019 <maneprathamesh019@gmail.com>
2024-04-14 13:27:58 +0530

    404 added

--- app/not-found.js
+++ app/not-found.js
@@ -1,14 +1,20 @@
-import { Image } from '@nextui-org/react'
+import { Button } from '@nextui-org/react'
+import Link from 'next/link'
 import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
-      <Image
-            src="/bg5-1.svg"
-            alt="Not-Found"
-            className="w-full h-full"
-          />
+    <div className='h-[90vh] flex flex-col justify-center items-center'>
+      <img
+        src="/404.gif"
+        alt="Not-Found"
+        height={500}
+        width={500}
+        className="mx-auto my-auto"
+      />
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-[95%]">
+        <Link href="/"><Button variant='ghost' size='lg' color='secondary'  >Go Home</Button></Link>
+      </div>
     </div>
   )
 }
