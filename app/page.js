f597dac774fb96540ab9aea7a4ee3615a6e97099
12DeshmukhAish <113546938+12DeshmukhAish@users.noreply.github.com>
2024-04-14 11:11:13 +0530

    home page and about us content changes

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <>
       <div className="h-[90vh] flex flex-col items-center justify-center bg-gradient-to-br from-purple-400 to-blue-500">
         <main className="flex flex-col items-center justify-center px-8 md:px-16 lg:px-32 text-center mt-10 relative z-10">
-          <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">Welcome to Our Healthcare Platform</h1>
-          <p className="text-lg md:text-xl text-white mb-8">Empowering healthcare providers and improving patient care</p>
+          <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">Welcome to Your Centralised Health Record Management System</h1>
+          <p className="text-lg md:text-xl text-white mb-8">Your Privacy, Our Promise</p>
           <button className="bg-white text-blue-500 font-semibold px-6 py-3 rounded-full shadow-lg uppercase tracking-wide hover:bg-blue-600 hover:text-white transition duration-300">Get Started</button>
         </main>
         </div>
@@ -20,20 +20,21 @@ export default function Home() {
 <section className="relative h-[80vh] py-20 mt-20">
         <img src="/bg-18.svg" alt="Illustration" className="absolute inset-0 object-cover  h-[80%] mx-auto  " />
         <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <h2 className="text-3xl font-bold text-gray-800 mb-4">The Problems We're Solving</h2>
-          <ul className="text-lg text-gray-700">
-            <li className="mb-2">Medical Fitness Exam Fraud</li>
-            <li className="mb-2">Hospital Data Integrity</li>
-            <li className="mb-2">Survey Efficiency</li>
-            <li className="mb-2">Insurance Fraud</li>
-          </ul> </div>
+          <h2 className="text-3xl font-bold text-gray-800 mb-4">Why Choose SwasthyaSamriddhi?</h2>
+        
+            <p className="text-lg text-gray-700">Centralised Health Record Management: Access, manage, and share your health records anytime, anywhere, eliminating the need for physical documents and ensuring continuity of care.<br></br>
+
+Secure & Confidential: We prioritize the highest standards of data protection and privacy with robust encryption, secure servers, and strict adherence to healthcare regulations like HIPAA, ensuring the confidentiality and integrity of your health information.<br></br>
+
+Equitable Access to Healthcare: We are dedicated to bridging the healthcare gap by providing our services to every individual, especially in rural areas, empowering them with access to quality healthcare services and resources.<br></br>
+</p></div>
       </section>
 
       <section className="relative h-[80vh] py-20 mt-10">
         <img src="/bg-14.svg" alt="Illustration" className="absolute inset-0 object-cover  h-[70%] mx-auto  " />
         <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <h2 className="text-3xl font-bold text-gray-800 mb-4">Overview</h2>
-          <p className="text-lg text-gray-700">The solution enhances healthcare by addressing these key challenges. It provides a comprehensive platform that tackles medical fitness exam fraud, ensures hospital data integrity, optimizes survey efficiency, and mitigates insurance fraud. By leveraging advanced technologies and robust security measures, our platform empowers healthcare organizations to overcome these obstacles effectively.
+          <h2 className="text-3xl font-bold text-gray-800 mb-4">Join Us</h2>
+          <p className="text-lg text-gray-700">Be Part of the Future of Healthcare: Join our growing community, explore our innovative services, and experience the future of healthcare with SwasthyaSamriddhi. Sign up today, stay connected, and take the first step towards empowered, personalized, and seamless healthcare access across India.
           </p>
         </div>
       </section>
