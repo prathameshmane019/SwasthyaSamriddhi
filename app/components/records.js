15cb7577749ddd523a786bff3df3e5ce54c8a53b
prathameshmane019 <maneprathamesh019@gmail.com>
2024-04-16 20:59:03 +0530

    Final touch

--- app/components/records.js
+++ app/components/records.js
@@ -2,28 +2,48 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import moment from 'moment';
+import {IdentificationIcon ,UserGroupIcon, UserIcon, CakeIcon,ClipboardListIcon } from '@heroicons/react/solid';
+import { GiBodyHeight } from "react-icons/gi";
+import { FaWeight } from "react-icons/fa";
 import { useSearchParams } from 'next/navigation';
-import { Card, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Spinner, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+import {  Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Spinner, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure,Image } from "@nextui-org/react";
 import {toast} from 'sonner'
 import RecordSkeleton from './skeleton/record';
+
+
 export default function UserRecords() {
   const searchParam = useSearchParams();
   const id = searchParam.get('id');
   const [isLoading, setIsLoading] = useState(true);
   const [selectedRecord, setSelectedRecord] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
+  const [user,setUser] =useState("");
 
   const [userHealthRecords, setUserHealthRecords] = useState([]);
+
+  const calculateAge = (dob) => {
+    const dobDate = new Date(dob);
+    const currentDate = new Date();
+    let age = currentDate.getFullYear() - dobDate.getFullYear();
+    const monthDiff = currentDate.getMonth() - dobDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < dobDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
   useEffect(() => {
     if (id) {
       const fetchRecords = async () => {
         setIsLoading(true);
         try {
           const response = await axios.post("/api/records/userrecords", { id });
-          setUserHealthRecords(response.data.map(record => ({
+          setUserHealthRecords(response.data.decryptedRecords.map(record => ({
             ...record,
             visitDate: moment(record.createdAt).format('DD/MM/YY') // Extract visit date from created date
           })));
+          setUser(response.data.user)
+          console.log(user);
           setIsLoading(false);
         } catch (error) {
           console.error("Error fetching records:", error);
@@ -42,9 +62,82 @@ export default function UserRecords() {
     setModalOpen(true);
     onOpen();
   };
-
+  const handleImageClick = () => {
+    window.open(selectedRecord.image.image_url, '_blank');
+  };
   return (
-    <> {isLoading ? ( 
+    <div className='h-screen overflow-y-scroll'> {user && (
+      <>
+      <div className=" rounded-lg shadow-lg overflow-hidden mt-10 mx-5">
+        <h2 className="text-xl font-bold text-center py-3 flex items-center justify-center">
+          <UserIcon className="mr-2 h-8 w-8 text-white" />
+          User Profile
+        </h2>
+        <div className="px-3 py-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className=" rounded-lg p-2 border-1 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+              <div className="flex items-center mb-2">
+                <UserIcon className="text-blue-500 mr-2 h-6 w-6" />
+                <p className="text-gray-600 font-medium">Full Name</p>
+              </div>
+              <p className="">{user.fullname.firstName} {user.fullname.surName}</p>
+            </div>
+
+            <div className=" rounded-lg p-2 border-1 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+              <div className="flex items-center mb-2">
+                <ClipboardListIcon className="text-blue-500 mr-2 h-6 w-6" />
+                <p className=" font-medium">Medication</p>
+              </div>
+              <p className="">{user.medication.name}</p>
+            </div>
+
+            <div className=" rounded-lg p-2 border-1 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+              <div className="flex items-center mb-2">
+                <IdentificationIcon className="text-blue-500 mr-2 h-6 w-6" />
+                <p className=" font-medium">ID</p>
+              </div>
+              <p className="">{user._id}</p>
+            </div>
+
+            <div className=" rounded-lg p-2 border-1 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+            <div className="flex items-center mb-2">
+        <CakeIcon className="text-blue-500 mr-2 h-6 w-6" />
+        <p className=" font-medium">Age</p>
+      </div>
+      <p className="">{calculateAge(user.dob)}</p></div>
+
+            <div className=" rounded-lg p-2 border-1 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+              <div className="flex items-center mb-2">
+                <UserGroupIcon className="text-blue-500 mr-2 h-6 w-6" />
+                <p className=" font-medium">Gender</p>
+              </div>
+              <p className="">{user.gender}</p>
+            </div>
+            
+            <div className=" rounded-lg p-2 border-1 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+              <div className="flex items-center mb-2">
+                <GiBodyHeight className="text-blue-500 mr-2 h-6 w-6" />
+                <p className=" font-medium">Height</p>
+              </div>
+              <p className="">{user.height}</p>
+            </div>
+            <div className=" rounded-lg p-2 border-1 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+              <div className="flex items-center mb-2">
+                <FaWeight className="text-blue-500 mr-2 h-6 w-6" />
+                <p className=" font-medium">Weight</p>
+              </div>
+              <p className="">{user.weight}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+    )
+
+    }
+    
+    
+    {isLoading ? ( 
       <div className='mt-10'>
       <RecordSkeleton />
       <RecordSkeleton />
@@ -61,8 +154,13 @@ export default function UserRecords() {
       <RecordSkeleton />
       <RecordSkeleton />
       </div>
-    ) : (
+    ) : (<>
+    <h2 className="text-xl font-bold text-center py-3 flex items-center justify-center">
+          <ClipboardListIcon className="mr-2 h-8 w-8 text-white" />
+          User Records
+        </h2>
       <Table
+      className='my-5 mx-5 '
         aria-label="User health records table"
         css={{
           "&.table": {
@@ -92,7 +190,7 @@ export default function UserRecords() {
           </TableColumn>
         </TableHeader>
         <TableBody
-        className='overflow-y-scroll'
+        className=''
           items={userHealthRecords}
           isLoading={isLoading}
           loadingComponent={<Spinner label="Loading records..." />}
@@ -103,21 +201,22 @@ export default function UserRecords() {
             </TableRow>
           )}
         </TableBody>
-      </Table>)}
+      </Table>
+      </>)}
       {selectedRecord && (
         <Modal
           backdrop="opaque"
           isOpen={isOpen}
           onOpenChange={onClose}
           classNames={{
-            backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20"
+            backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20 overflow-y-scroll "
           }}
         >
-          <ModalContent>
+          <ModalContent className='mt-36 h-[80vh] overflow-y-scroll'>
             {(onCloseModal) => (
               <>
                 <ModalHeader className="flex flex-col gap-1">Health Record Details</ModalHeader>
-                <ModalBody className='m-0'>
+                <ModalBody className='m-0 '>
                   <div className='m-0'>
                     <div className="mb-4">
                       <label htmlFor="doctorId" className="block text-gray-700 font-bold mb-2">
@@ -167,7 +266,8 @@ export default function UserRecords() {
                         {selectedRecord.notes}
                       </p>
                     </div>
-                  </div>
+                    {selectedRecord.image && <div onClick={handleImageClick} style={{ cursor: 'pointer' }}> <Image src={selectedRecord.image.image_url} alt="record file" /></div>}
+                </div>
                 </ModalBody>
                 <ModalFooter>
                   <Button color="danger" variant="light" onClick={onCloseModal}>
@@ -179,6 +279,6 @@ export default function UserRecords() {
           </ModalContent>
         </Modal>
       )}
-    </>
+    </div>
   );
 }
